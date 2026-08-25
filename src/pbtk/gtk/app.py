8914a5c345ad2696b7189ab03baded933e2a6df1
Marin <marin-m@users.noreply.github.com>
2026-08-25 20:59:12 +0200

    feat/gtk-gui: Precise file structure

---
+++ src/pbtk/gtk/app.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def main():
+    pass # (⚠️ WIP: Initialize GTK 4 app)
+
+    # (⚠️ TODO: Load MainWindow from main_window.py)
+
+if __name__ == '__main__':
+    main()
--- src/pbtk/gtk/main_window.py
+++ src/pbtk/gtk/main_window.py
@@ -1,2 +1,3 @@
 #!/usr/bin/env python3
 
+# (⚠️ TODO: Load from BLP file)
